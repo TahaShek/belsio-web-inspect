c0f32ac6070a4e44dd6d622be44bb76fd4cff244
TahaShek <sohailtaha128@gmail.com>
2023-10-31 15:07:41 +0500

    Sfdsf

--- src/app/core/interceptors/index.ts
+++ src/app/core/interceptors/index.ts
@@ -2,11 +2,13 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { JwtInterceptor } from './jwt.interceptor';
 import { ResponseInterceptor } from './response.interceptor';
 import { UrlInterceptor } from './url.interceptor';
+import { RequestInterceptor } from './request.interceptor';
 
 export const InterceptorProviders = [
   { provide: HTTP_INTERCEPTORS, useClass: UrlInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: ResponseInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: RequestInterceptor, multi: true },
   // { provide: HTTP_INTERCEPTORS, useClass: RetryInterceptor, multi: true },
   // { provide: HTTP_INTERCEPTORS, useClass: CachingInterceptor, multi: true },
 ];
